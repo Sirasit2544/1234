846f4a314593c957fc4135fe94879c74fb9916fe
Sirasit2544 <46451950+Sirasit2544@users.noreply.github.com>
2019-03-15 21:36:47 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* recomended 40 sec */
+$set_delay=40;
+
+$config=[
+"imei" => "352812106762513",
+"sign" => "7e5f3962407f05a3ec318b8e47f211296f62cfb5",
+"uuid" => "a383c0d28c20ec5a",
+"token" => "06b562f9596db5ce2da6582f31894a74"
+];
